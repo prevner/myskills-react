a269ece9f89de787f4d642a817315d46d1c081bb
Dixy Prevner M'POUTOU BABINGUI <dixy-prevner.m-poutou-babingui@estiam.com>
2023-04-04 16:24:36 +0200

    with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Content from './components/Content/Content';
+
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id :1,
+      checked : true,
+      item : "First item"
+    },
+    {
+      id :2,
+      checked : false,
+      item : "Second item"
+    },
+    {
+      id :3,
+      checked : false,
+      item : "Third item"
+    }
+  ]);
+  
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist',JSON.stringify(listItems))
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist',JSON.stringify(listItems))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header 
+          title={"Groceries list"}
+        />
+       <Content
+       items={items}
+           handleCheck={handleCheck}
+           handleDelete={handleDelete}
+       />
+       <Footer 
+          length={items.length} 
+        />
     </div>
   );
 }
